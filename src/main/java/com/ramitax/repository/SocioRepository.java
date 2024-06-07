09aaead4303b6afa3f1de6a25a2272f981a4bac6
Ramiro Mendoza <ramamendoza@gmail.com>
2024-06-07 13:44:10 -0300

    Creacion de la intefaz SocioRepository

---
+++ src/main/java/com/ramitax/repository/SocioRepository.java
@@ -0,0 +1,7 @@
+package com.ramitax.repository;
+
+import com.ramitax.model.Socio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocioRepository extends JpaRepository<Socio, Integer> {
+}
