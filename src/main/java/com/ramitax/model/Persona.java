e3b99bf3cb4f71d6cec9f987606b21e7a69c08f2
Ramiro Mendoza <ramamendoza@gmail.com>
2024-06-07 10:50:25 -0300

    Creacion de la superclase persona

---
+++ src/main/java/com/ramitax/model/Persona.java
@@ -0,0 +1,37 @@
+package com.ramitax.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String dni;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    @Column(nullable = false)
+    private String telefono;
+
+    @Column(nullable = false)
+    private String email;
+}
