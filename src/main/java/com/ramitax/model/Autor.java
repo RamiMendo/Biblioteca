568b641d50ba41f9780e8b379311456734e44315
Ramiro Mendoza <ramamendoza@gmail.com>
2024-06-07 10:51:24 -0300

    Creacion de la entidad Autor

---
+++ src/main/java/com/ramitax/model/Autor.java
@@ -0,0 +1,18 @@
+package com.ramitax.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "autor")
+@Entity
+public class Autor extends Persona {
+
+}
