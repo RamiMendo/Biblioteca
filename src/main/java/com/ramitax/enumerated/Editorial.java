61430436177408ec4106af7ce8334961f6176a8c
Ramiro Mendoza <ramamendoza@gmail.com>
2024-06-07 12:05:10 -0300

    Creacion del enumerado Editorial

---
+++ src/main/java/com/ramitax/enumerated/Editorial.java
@@ -0,0 +1,16 @@
+package com.ramitax.enumerated;
+
+public enum Editorial {
+
+    RA_MA,ANAYA,Mc_Graw_Hill,Prentice_Hall,
+    MP_ediciones,
+    McGRAW_HILL,
+    Piramide_Anaya,
+    Manantial,Espacio,
+    Amorrortu_editores,
+    Edibesa,Alfaomega,
+    Laertes,
+    La_Biblia,
+    Anaya_Multimedia,
+    Paperback
+}
